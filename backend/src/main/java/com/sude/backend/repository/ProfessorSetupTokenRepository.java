1de44a0415bdc36d30816f0ea42d4d6521bf19b0
Sudenaz Çakır <22070006025@stu.yasar.edu.tr>
2026-04-01 01:37:17 +0300

    temp: setup token implementation

--- backend/src/main/java/com/sude/backend/repository/ProfessorSetupTokenRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sude.backend.repository;
-
-import com.sude.backend.entity.ProfessorSetupToken;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface ProfessorSetupTokenRepository extends JpaRepository<ProfessorSetupToken, Long> {
-    Optional<ProfessorSetupToken> findByToken(String token);
-}
