828153926305b32f9a1efcb46b1ea0292177dafc
Osman Sahin Guler <24070006009@stu.yasar.edu.tr>
2026-04-13 00:57:05 +0300

    Revert "merge: fix group creation notification failures"
    This reverts commit bf39ac52d1f1de12310f2ba69ce458c8dd26eda4, reversing
    changes made to 87cb4c4cbaa96060778b1bf77213119e8de4808c.

--- backend/models/Group.js
+++ backend/models/Group.js
@@ -7,20 +7,10 @@ const Group = sequelize.define(
     id: {
       type: DataTypes.STRING,
       primaryKey: true,
-      defaultValue: () => `${Date.now()}${Math.floor(Math.random() * 1000)}`,
     },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
-    },
-    groupName: {
-      type: DataTypes.VIRTUAL,
-      get() {
-        return this.getDataValue('name');
-      },
-      set(value) {
-        this.setDataValue('name', value);
-      },
     },
     leaderId: {
       type: DataTypes.STRING,
@@ -30,25 +20,6 @@ const Group = sequelize.define(
       type: DataTypes.JSON,
       allowNull: false,
       defaultValue: [],
-    },
-    members: {
-      type: DataTypes.VIRTUAL,
-      get() {
-        return this.getDataValue('memberIds') || [];
-      },
-      set(value) {
-        this.setDataValue('memberIds', Array.isArray(value) ? value : []);
-      },
-    },
-    maxMembers: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      defaultValue: 4,
-    },
-    status: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      defaultValue: 'FORMATION',
     },
     advisorId: {
       type: DataTypes.STRING,
