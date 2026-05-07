7cfc08deb2305c9fe1994c9fa633a72145b97c09
OsmanOzan <osmanozankavakli@gmail.com>
2026-05-07 23:02:39 +0300

    Resolve merge conflicts in models/index.js and routes/finalEvaluation.js

--- backend/routes/finalEvaluation.js
+++ backend/routes/finalEvaluation.js
@@ -1,38 +1,9 @@
 'use strict';
 
 const express = require('express');
-const { authenticate, authorize } = require('../middleware/auth');
-const {
-  groupIdValidation,
-  postTeamScalar,
-  getTeamScalarHandler,
-  getContributionsHandler,
-} = require('../controllers/finalEvaluationController');
-
 const router = express.Router();
+const { submitAdvisorGradeValidation, postAdvisorGrade } = require('../controllers/finalEvaluationController');
 
-router.post(
-  '/groups/:groupId/team-scalar',
-  authenticate,
-  authorize(['COORDINATOR']),
-  groupIdValidation,
-  postTeamScalar,
-);
-
-router.get(
-  '/groups/:groupId/team-scalar',
-  authenticate,
-  authorize(['COORDINATOR', 'PROFESSOR']),
-  groupIdValidation,
-  getTeamScalarHandler,
-);
-
-router.get(
-  '/groups/:groupId/contributions',
-  authenticate,
-  authorize(['COORDINATOR', 'PROFESSOR', 'ADMIN']),
-  groupIdValidation,
-  getContributionsHandler,
-);
+router.post('/advisor/:groupId', submitAdvisorGradeValidation, postAdvisorGrade);
 
 module.exports = router;
